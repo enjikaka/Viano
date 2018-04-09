e64dffee1da96076129969978d6bcbc749afd40e
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2018-04-09 19:11:12 +0200

    Add PiSong

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <la-cucaracha></la-cucaracha>
+    <pi-song></pi-song>
   </div>
 </template>
 
 <script>
 import LaCucaracha from './examples/LaCucaracha.vue'
+import PiSong from './examples/PiSong.vue'
 
 export default {
   name: 'app',
   components: {
-    LaCucaracha
+    LaCucaracha,
+    PiSong
   }
 }
 </script>
